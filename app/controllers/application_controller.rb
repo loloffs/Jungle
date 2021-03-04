4723e837a5dcd399b593891c8fc22793cbeff60f
loloffs <lukeoloffs@gmail.com>
2021-03-04 00:24:28 +0000

    Added basic http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  # include HttpAuthConcern
+
+  http_basic_authenticate_with name: ENV.username, password: ENV.password
+
+
   protect_from_forgery with: :exception
 
   private
