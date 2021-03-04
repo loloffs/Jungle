0875f66951992adbffe4126a28b6c96f5d39b284
loloffs <lukeoloffs@gmail.com>
2021-03-04 16:23:50 +0000

    Created about page

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,10 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  # get '/about' => 'about#view'
+
+  get '/about', to: 'about#index'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
