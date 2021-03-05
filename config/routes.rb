1ba3f13804d0db5586b925a62097d35b71d69b73
loloffs <lukeoloffs@gmail.com>
2021-03-05 00:25:14 +0000

    Added admin categories page

--- config/routes.rb
+++ config/routes.rb
@@ -70,9 +70,9 @@ Rails.application.routes.draw do
   #   resources :photos, concerns: :toggleable
 
   # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    namespace :admin do
+      # root to: 'categories#index'
+      resources :categories
+      # get 'admin/categories', to: 'admin#categories'
+    end
 end
